f8880dbcfb1df2909441ecbdee61a65e21a61bf8
deltaflyer4747 <andys@deltaflyer.cz>
2017-04-01 23:08:27 +0200

    typo 2

--- test.py
+++ test.py
@@ -12,17 +12,11 @@ DIRT_PORT = 20777
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
 sock.bind((DIRT_IP, DIRT_PORT))
 
-#print "UDP MON RDY @", DIRT_IP, ":", DIRT_PORT
-
-
-#filek = open("dump.txt","r").read().split("\n")
-
-
 while True:
 	data, addr = sock.recvfrom(512)
 
 	outsim_pack = struct.unpack('64f', data[0:256])
-	print "%s %s %s %s %s %s" %(outsim_pack[11-17])
+	print "%s %s %s %s %s %s" %(outsim_pack[11:17])
 #	for id, value in enumerate(outsim_pack):
 #		print "%d : %s" % (id, value)
 
